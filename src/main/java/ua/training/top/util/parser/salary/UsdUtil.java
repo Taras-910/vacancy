de9c1bd15e0575d893d056abf37e5f827c61dc5d
taras <promverstat@ukr.net>
2021-10-16 00:25:50 +0300

    7.9 fit 2 wrong usdUtil

--- src/main/java/ua/training/top/util/parser/salary/UsdUtil.java
+++ src/main/java/ua/training/top/util/parser/salary/UsdUtil.java
@@ -9,31 +9,29 @@ public class UsdUtil {
     public static final Logger log = LoggerFactory.getLogger(UsdUtil.class);
 
     public static String getUsd(String temp){
-        temp = temp.replace("$", "").concat("$");
-        temp = temp.replace("usd", "").concat("$");
+        temp = temp.replaceAll("$", "").concat("$");
+        temp = temp.replaceAll("usd", "").concat("$");
         String cleaned = getCleaned(temp.substring(0, temp.contains("$") ? temp.indexOf("$") : temp.indexOf("usd")));
-//        String cleaned = temp.indexOf("$") == 0 ? temp.substring(1)
-//                : getCleaned(temp.substring(0, temp.contains("$") ? temp.indexOf("$") : temp.indexOf("usd")));
+        cleaned = correctIfYear(temp, cleaned);
         if (!temp.contains("грн")){
             if(cleaned.contains("—")){
-                if (temp.contains("рік") || temp.contains("год")/* || temp.contains("year")*/) {
-                    return String.valueOf(Integer.parseInt(cleaned.split("\\W")[0]) / 12).concat("00-")
-                            .concat(String.valueOf(Integer.parseInt(cleaned.split("\\W")[1]) / 12)).concat("00");
-                }
                 return cleaned.replace("—", "00—").concat("00");
             } else{
-                return temp.contains("от") ? cleaned.concat("00—").concat("1") :"1".concat("—").concat(cleaned).concat("00");
+                return temp.contains("от") ? cleaned.concat("00—").concat("1") : "1".concat("—").concat(cleaned).concat("00");
             }
         } else {
             if (temp.contains("—")) {
-                if (temp.contains("рік") || temp.contains("год")/* || temp.contains("year")*/) {
-                    return String.valueOf(Integer.parseInt(cleaned.split("\\W")[0]) / 12).concat("00-")
-                            .concat(String.valueOf(Integer.parseInt(cleaned.split("\\W")[1]) / 12)).concat("00");
-                }
                 return cleaned.substring(temp.indexOf("$") + 1).replace("—", "00—").concat("00");
             } else {
                 return temp.contains("·") ? "1".concat("—").concat(temp.substring(temp.indexOf("$") + 1)).concat("00") : cleaned;
             }
         }
+    }
+
+    private static String correctIfYear(String temp, String cleaned) {
+        return temp.contains("рік") || temp.contains("год") || temp.contains("year") ? (temp.contains("—") ?
+                String.valueOf(Integer.parseInt(cleaned.split("\\W")[0]) / 12).concat("—")
+                        .concat(String.valueOf(Integer.parseInt(cleaned.split("\\W")[1]) / 12)) :
+                String.valueOf(Integer.parseInt(cleaned) / 12)) : cleaned;
     }
 }
